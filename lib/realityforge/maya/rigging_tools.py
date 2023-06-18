699feae051be1cc6fb4efc52d4b4f4d976fe7da4
Peter Donald <peter@realityforge.org>
2023-06-18 14:17:08 +1000

    Add script to automate the direct connection between a driver hierarchy and a driven hierarchy

---
+++ lib/realityforge/maya/rigging_tools.py
@@ -0,0 +1,72 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import maya.cmds as cmds
+from parse import parse
+
+
+# Note: to use this script you need to run
+# "C:\Program Files\Autodesk\Maya2023\bin\mayapy.exe" -m pip install --user parse
+
+def connect_transform_attributes(driver_object_name: str, driven_object_name: str) -> None:
+    """Connect the transform, rotate and scale attributes of the driver object to the driven object.
+
+    This is typically used to copy attributes from a driver joint into a driven joint so that the
+    joint hierarchy/skeleton can be cleanly exported for game engines.
+
+    :param driver_object_name: the name of the driver object.
+    :param driven_object_name:  the name of the driven object.
+    """
+    for attr in ["translate", "rotate", "scale"]:
+        cmds.setAttr(f"{driven_object_name}.{attr}", lock=False)
+        cmds.connectAttr(f"{driver_object_name}.{attr}", f"{driven_object_name}.{attr}", lock=True, force=True)
+
+
+def connect_transform_attributes_in_hierarchy(name: str,
+                                              driver_object_name_pattern: str,
+                                              driven_object_name_pattern: str) -> int:
+    """Connect the transform, rotate and scale attributes of the driver object to the driven object.
+    :param name: The "base" name of the object.
+    :param driver_object_name_pattern: the f-string pattern used to derive the driver name.
+    :param driven_object_name_pattern: the f-string pattern used to derive the driven name.
+    :return: The number of objects matched.
+    """
+    driver_name = driver_object_name_pattern.format(name=name)
+    driver = cmds.ls(driver_name)
+    if 0 == len(driver):
+        return 0
+    elif 1 != len(driver):
+        raise Exception(f"Multiple driver objects detected with the name {driver_name}. Aborting!")
+
+    driven_name = driven_object_name_pattern.format(name=name)
+    driven = cmds.ls(driven_name)
+    if 0 == len(driven):
+        return 0
+    elif 1 != len(driven):
+        raise Exception(f"Multiple driven objects detected with the name {driven_name}. Aborting!")
+
+    connect_transform_attributes(driver_name, driven_name)
+
+    transformed = 1
+
+    children = cmds.listRelatives(driven_name)
+    if children:
+        for child in children:
+            match = parse(driven_object_name_pattern, child)
+            if match:
+                child_base_name = match.named.get("name")
+                if child_base_name:
+                    transformed += connect_transform_attributes_in_hierarchy(child_base_name,
+                                                                             driver_object_name_pattern,
+                                                                             driven_object_name_pattern)
+
+    return transformed
