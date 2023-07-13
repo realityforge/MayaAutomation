7352759086994bfaf939d4129079cefe2b92cad3
Peter Donald <peter@realityforge.org>
2023-07-13 13:43:35 +1000

    Copy a few more attributes

--- lib/realityforge/maya/basic_rigger.py
+++ lib/realityforge/maya/basic_rigger.py
@@ -165,10 +165,13 @@ def process_joint(rigging_settings: RiggingSettings,
                                  # Joint Labelling
                                  "side",
                                  "type",
+                                 "otherType",
                                  "drawLabel",
                                  # Drawing Overrides
                                  "overrideEnabled",
                                  "overrideColor",
+                                 "overrideColorRGB",
+                                 "overrideRGBColors",
                                  "overridePlayback",
                                  "overrideShading",
                                  "overrideVisibility",
