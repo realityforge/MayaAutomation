0b11642e7c512a6d0c8f3885b43f932872fa16ba
Peter Donald <peter@realityforge.org>
2023-07-23 17:39:40 +1000

    Optimise imports

--- lib/realityforge/maya/basic_rigger.py
+++ lib/realityforge/maya/basic_rigger.py
@@ -9,13 +9,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import math
 from typing import Optional
 
 import maya.api.OpenMaya as om
 import maya.cmds as cmds
 from parse import parse
-import math
 
 import realityforge.maya.util as util
 
